868235e426c7dd0783a3845deee563b65970be54
Sebastian Sickelmann <sickelcoding@googlemail.com>
2011-10-05 05:56:34 +0200

    Initial Import that shows error in invoke dynamic handling in eclipse 3.7.1

---
+++ jdk/invokedynamic/helloDyn/src/GEN.java
@@ -0,0 +1,41 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.MethodHandle;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+public class GEN {
+	public static void main(String[] args) throws IOException {
+		ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES
+				| ClassWriter.COMPUTE_MAXS);
+		// cw.newClass("G");
+		cw.visit(51, Opcodes.ACC_PUBLIC, "G", null, "java/lang/Object", null);
+
+		
+		MethodVisitor mw = cw.visitMethod(Opcodes.ACC_PUBLIC
+				| Opcodes.ACC_STATIC, "call", "()V", null, null);
+		mw.visitCode();
+//		mw.visitMethodInsn(Opcodes.INVOKESTATIC, "M", "cafeBabe", "()V");
+		MethodHandle BSM_LEFT = new MethodHandle(
+				Opcodes.MH_INVOKESTATIC,
+				"M",
+				"bootstrap",
+				"(Ljava/lang/invoke/MethodHandles$Lookup;Ljava/lang/String;Ljava/lang/invoke/MethodType;I)Ljava/lang/invoke/CallSite;");
+		mw.visitInvokeDynamicInsn("dyn", "()V", BSM_LEFT, 1);
+		mw.visitInsn(Opcodes.RETURN);
+		mw.visitMaxs(0, 0);
+
+		mw.visitEnd();
+		cw.visitEnd();
+
+		byte[] byteArray = cw.toByteArray();
+		System.out.println(byteArray.length);
+		FileOutputStream fout = new FileOutputStream("gen/G.class");
+		fout.write(byteArray);
+		fout.close();
+	}
+}
---
+++ jdk/invokedynamic/helloDyn/src/M.java
@@ -0,0 +1,26 @@
+import java.lang.invoke.CallSite;
+import java.lang.invoke.ConstantCallSite;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles.Lookup;
+import java.lang.invoke.MethodType;
+
+public class M {
+	public static void main(String[] args) throws ClassNotFoundException {
+		System.out.println("START");
+		G.call();
+		System.out.println("END");
+	}
+
+	public static void cafeBabe() {
+		System.out.println("DONE");
+	}
+
+	public static CallSite bootstrap(Lookup lookup, String name,
+			MethodType type, int rightValue) throws NoSuchMethodException, IllegalAccessException {
+
+		MethodHandle printArgs = lookup.findStatic(M.class, "cafeBabe",
+				MethodType.methodType(void.class));
+		System.out.println("Bootstrap");
+		return new ConstantCallSite(printArgs.asType(type));
+	}
+}
