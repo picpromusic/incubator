1ec942cdb0f8526f15c0ff5c4f95b819e771ed0e
Sebastian Sickelmann <sickelcoding@googlemail.com>
2012-01-22 21:53:47 +0100

    Send all if tag does not exist

--- misc/git-shell-skripts/send_to.sh
+++ misc/git-shell-skripts/send_to.sh
@@ -1,9 +1,18 @@
-export BUNDLEOUT=`git config --get sync.$1.bundleout`
-export TEMP=`ls $BUNDLEOUT | sort -u | tail -1 | cut -d "." -f1`
-export TEMP=`echo $TEMP | sed 's/0*//'`
+BUNDLEOUT=`git config --get sync.$1.bundleout`
+TAGOUT=`git config --get sync.$1.out`
+TEMP=`ls $BUNDLEOUT | sort -u | tail -1 | cut -d "." -f1`
+TEMP=`echo $TEMP | sed 's/0*//'`
 declare -i MAX=$TEMP
 declare -i NEXT=$MAX+1
 declare T=`seq -f %09.0f $NEXT $NEXT`.bundle
-git bundle create $BUNDLEOUT/$T HEAD `git config --get sync.$1.out`
+TEMP='TEMP'
+TEMP=`git show $TAGOUT | grep commit | head -1 | cut -d " " -f 2 | wc -l`
+echo $TEMP
+echo $TAGOUT
+if [ $TEMP != '1' ]; then
+ git bundle create $BUNDLEOUT/$T HEAD
+else
+ git bundle create $BUNDLEOUT/$T HEAD `git config --get sync.$1.out`
+fi
 git tag -f `git config --get sync.$1.out`
 echo $T
