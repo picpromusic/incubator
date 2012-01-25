3fc8a7b40b4c3eae139f0c457f0faf572a86e75a
Sebastian Sickelmann <sickelcoding@googlemail.com>
2012-01-25 04:52:38 +0100

    ver

--- misc/git-shell-skripts/conf_sync.sh
+++ misc/git-shell-skripts/conf_sync.sh
@@ -1,47 +1,95 @@
-ISSET=$1'notset'
-if [ $ISSET != 'notset' ]; then
-
- if [ $1 = '-self' ]; then
-  ISSET=$2'notset'
-  if [ $ISSET = 'notset' ]; then
-   echo "Usage $0 -self own_name"
-   exit
-  fi
-  git config sync.self.name=$2
+function global_usage_info() {
+  echo "Usage $0 -self --> to set own name"
+  echo "Usage $0 -other --> to setup other syncrepo"
+  echo "Usage $0 -key --> to setup a gpg-key"
+  echo "Usage $0 -check -> to check for minimal configuration"
+  echo "Usage $0 --help --> to display this message"
   exit
- fi
+}
 
- if [ $1 = '-other' ]; then
-  ISSET=$2'notset'
-  if [ $ISSET != 'notset' ]; then
-   ISSET=$3'notset'
-   if [ $ISSET != 'notset' ]; then
-    git config sync.$2.in=$2_in
-    git config sync.$2.out=$2_out
-    git config sync.$2.bundlein=$3/in
-    git config sync.$2.bundleout=$3/out
-    exit
-   fi
-  fi
+function self_usage_info() {
+  echo "Usage $0 -self own_name"
+  exit
+}
+
+function other_usage_info() {
   echo "Usage $0 -other other_name abs_bundle_in_out_dir"
   exit
- fi
+}
 
- if [ $1 = '-key' ]; then
-  ISSET=$2'notset'
-  if [ $ISSET != 'notset' ]; then
-   ISSET=$3'notset'
-   if [ $ISSET != 'notset' ]; then
-    git config sync.$2.signkey=$3
-    exit
-   fi
-  fi
+function key_usage_info() {
   echo "Usage $0 -key other_name keyname" 
   exit
- fi
+}
 
+function single_check() {
+  if !(grep -q "$2" - <<< $1); then
+    echo [$3] configuration does not contain $2
+#    if [ $3 = 'error' ]; then
+#      exit
+#    fi
+  fi
+}
+
+function check() {
+  echo checking configuration for $1
+  conf=$(git config -l | grep sync)
+  single_check "$conf" sync.self.name error
+  single_check "$conf" sync.$1.in error
+  single_check "$conf" sync.$1.out error
+  single_check "$conf" sync.$1.bundlein error
+  single_check "$conf" sync.$1.bundleout error
+  single_check "$conf" sync.$1.signkey warning
+}
+
+
+function checkall() {
+  conf=$(git config -l | grep sync | grep -v sync.self.name | cut -d "." -f 2 | sort -u)
+  confs=''
+  for conf_name in $conf; do confs=$confs,$conf_name; done
+  echo "checking the following configurations (${confs:1})"
+  for conf_name in $conf; do check $conf_name; done
+}
+
+
+if [ $# -lt 1 ]; then
+  git config -l | grep sync
+  exit
 fi
 
-echo "Usage $0 -self --> to set own name"
-echo "Usage $0 -other --> to setup other syncrepo"
-echo "Usage $0 -key --> to setup a gpg-key"
+case "$1" in
+-self )
+  if [ $# -ne 2 ]; then
+    self_usage_info
+  fi
+  git config sync.self.name $2
+  ;;
+-other )
+  if [ $# -ne 3 ]; then
+    other_usage_info
+  fi
+  git config sync.$2.in $2_in
+  git config sync.$2.out $2_out
+  git config sync.$2.bundlein $3/in
+  git config sync.$2.bundleout $3/out
+  ;;
+-key )
+  if [ $# -ne 3 ]; then
+    key_usage_info
+  fi
+  git config sync.$2.signkey $3
+  ;;
+-check )
+  if [ $# -eq 2 ]; then
+    check $2
+  else
+    checkall
+  fi
+  ;;
+--help )
+  global_usage_info
+  ;;
+* )
+  global_usage_info
+  ;;
+esac
